6297d35b282c92e2c07e92584eb9cfbae306190b
CodeeBy <khamidullayevammoriddin@gmail.com>
2025-04-28 23:23:07 +0845

    About page added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,39 @@
 import { title } from "@/components/primitives";
+import Link from "next/link";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1 className={title()}>About</h1>
+    <div className="flex flex-col gap-8">
+      <div className="inline-block max-w-5xl text-left justify-center">
+        <h1 className={title()}>About the project</h1>
+        <div className="my-8">
+          <p className="text-lg text-default-600">
+            This project was created by Ammoriddin (khamidullayevdev). Its primary goal is to help people easily 
+            <span className="text-primary font-medium">build stunning portfolios.</span> 
+            You simply enter your information, and the website will generate a portfolio for you based on your chosen design. 
+            The platform is <span className="text-primary font-medium">free to use,</span> although premium subscription options are also available.
+          </p>
+        </div>
+
+        <h2 className={title({ size: 'sm', className: 'mt-12' })}>Getting Started</h2>
+        <div className="my-4">
+          <p className="text-lg text-default-600">
+            First, you need to <Link href={'/signup'} className="text-primary font-medium hover:underline">register</Link> and <Link href={'/login'} className="text-primary font-medium hover:underline">log in</Link> to your account.
+            After that, you can visit the <Link href={'/'} className="text-primary font-medium hover:underline">Tutorial</Link> section to view the user guide. 
+            If you would like to support the project, you are welcome to make a donation.
+          </p>
+        </div>
+
+        <h2 className={title({ size: 'sm', className: 'mt-12' })}>About the Creator</h2>
+        <div className="my-4">
+          <p className="text-lg text-default-600">
+            Hello, I am the creator of this project, and I am from Uzbekistan.
+            I started developing this project on <a className="text-primary font-medium hover:underline" href="https://github.com/ammoriddin">Github</a> April 27, 2025, and I am continuously working to improve it. 
+            To learn more about me, you can visit my <a className="text-primary font-medium hover:underline" href="https://github.com/ammoriddin">Github</a> page. 
+            I hope you enjoy using this platform!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
         <br />
         <span className={title()}>
-          websites regardless of your design experience.
+          portfolio websites regardless of your design experience.
         </span>
         <div className={subtitle({ class: "mt-4" })}>
           Beautiful, fast and modern React UI library.
