25cb9961b9a52814d7c68faa524351d29ec643c5
CodeeBy <khamidullayevammoriddin@gmail.com>
2025-05-05 17:03:16 +0500

    f

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qmnjuxylghuaonfrdcuu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtbmp1eHlsZ2h1YW9uZnJkY3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NDU2MTgsImV4cCI6MjA2MTQyMTYxOH0.sxTKnONudlg2ejq78tU2osgozJykUQExjNDTav04e1I';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
